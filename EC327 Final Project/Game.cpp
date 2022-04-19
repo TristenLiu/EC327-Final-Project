3d6b8ae544e561ebdb891e0c08d7da9e78a76a7a
TristenLiu <tristenliu9876@gmail.com>
2022-04-19 17:09:23 -0400

    Added different windows & inputs to change windows

--- EC327 Final Project/Game.cpp
+++ EC327 Final Project/Game.cpp
@@ -5,12 +5,23 @@
 void Game::initVar()
 {
 	this->window = nullptr;
+	this->font.loadFromFile("fonts/arial.ttf");
+	this->background.loadFromFile("images/checkerboard2.png");
+	this->mainbg.setTexture(background);
+	this->button1.loadFromFile("images/button_select_level.png");
+	this->lvl1button.setTexture(button1);
+	this->grid.loadFromFile("images/grid_6.png");
+	this->playGrid.setTexture(grid);
+	this->playGrid.setScale(.1, .1);
+	this->playGrid.setPosition(200, 200);
+
+	this->gameState = 1; /*First game state is title menu*/
 }
 
 void Game::initWindow()
 {
-	this->videoMode.height = 600;
-	this->videoMode.width = 800;
+	this->videoMode.height = 800;
+	this->videoMode.width = 1280;
 
 	this->window = new sf::RenderWindow(this->videoMode, "Game", sf::Style::Titlebar | sf::Style::Close);
 }
@@ -45,12 +56,118 @@ void Game::pollEvents()
 		case sf::Event::KeyPressed:
 			if (this->ev.key.code == sf::Keyboard::Escape)	//change this to open a game menu when pressed.
 				this->window->close();
-			break;
+
+			//change all of these inputs to mouse clicks on screen buttons in the future
+			else if (this->gameState == 1)
+			{
+				if (this->ev.key.code == sf::Keyboard::Space) //Pressing space on title screen will change the game state to the game board
+				{
+					this->gameState = 2;
+				}
+			}
+			else if (this->gameState == 2)
+			{
+				if (this->ev.key.code == sf::Keyboard::Enter) //pressing Enter on the game brings you to the 'memo state'
+				{
+					this->gameState = 3;
+				}
+				else if (this->ev.key.code == sf::Keyboard::Tab) //pressing Tab will return to the title screen
+				{
+					this->gameState = 1;
+				}
+			}
+			else if (this->gameState == 3)
+			{
+				if (this->ev.key.code == sf::Keyboard::RShift) //pressing RShift in the 'memo state' brings you to back to the game
+				{
+					this->gameState = 2;
+				}
+			}
 		}
 	}
 }
 
 //Functions
+void Game::renderScreen()
+{
+	this->window->draw(this->mainbg);
+
+	if (this->gameState == 1) //game is at main menu
+	{
+		//display title logo 
+		//display buttons to select the mode (via mouse, can maybe include using the keyboard arrow keys as well)
+		//should we have a backdrop image?
+
+		sf::Text gameTitle;
+		gameTitle.setString("Voltorb Flip V2");
+		gameTitle.setFont(font);
+		gameTitle.setCharacterSize(80);
+		gameTitle.setFillColor(sf::Color::Blue);
+		gameTitle.setStyle(sf::Text::Bold | sf::Text::Underlined);
+		gameTitle.setPosition(250.f, 50.f);
+
+		lvl1button.setPosition(200.f, 200.f);
+
+		this->window->draw(gameTitle);
+		this->window->draw(lvl1button);
+	}
+
+	else if (this->gameState == 2)
+	{
+		sf::Text title; //title should show current level (and current total accumulated score if in marathon mode)
+		title.setFont(font);
+		title.setCharacterSize(60);
+		title.setFillColor(sf::Color::Blue);
+		title.setStyle(sf::Text::Bold);
+		title.setString("Level"); //stringstream
+		title.setPosition(200.f, 100.f);
+		this->window->draw(title);
+
+
+
+		/*
+		On the left we should have:
+		- display score for the current level being played
+		- memo where player can mark cards
+
+		on the unused square on grid, put a quit button?
+
+		*/
+
+
+		this->window->draw(playGrid);
+
+		//may need a separate state for when memo is being used
+	}
+
+	else if (this->gameState == 3)
+	{
+		sf::Text title; //title should show current level (and current total accumulated score if in marathon mode)
+		title.setFont(font);
+		title.setCharacterSize(70);
+		title.setFillColor(sf::Color::Magenta);
+		title.setStyle(sf::Text::Bold);
+		title.setString("Memo State"); //stringstream
+		title.setPosition(300, 100);
+		this->window->draw(title);
+
+		/*
+		On the left we should have:
+		- display score for the current level being played
+		- memo where player can mark cards
+		- consider tile class that can store values and textures, memo marks can be a variable in tile class
+
+		on the unused square on grid, put a quit button?
+
+		*/
+
+
+		this->window->draw(playGrid);
+
+		//may need a separate state for when memo is being used
+	}
+}
+
 void Game::update()
 {
 	//Event Polling
@@ -59,11 +176,10 @@ void Game::update()
 
 void Game::render()
 {
-	/*
-	Render game objects (window, tiles, scoreboard, etc.)
-	*/
-	this->window->clear(sf::Color(255, 255, 255, 255)); //change this to actual graphics window
+	//Render game objects(window, tiles, scoreboard, etc.)
+	this->window->clear(sf::Color::White); 
 
-	
+	this->renderScreen();
+
 	this->window->display();
 }
--- EC327 Final Project/Game.h
+++ EC327 Final Project/Game.h
@@ -20,6 +20,21 @@ private:
 	sf::RenderWindow* window;
 	sf::VideoMode videoMode;
 	sf::Event ev;
+	sf::Font font;
+	sf::Texture background;
+	sf::Sprite mainbg;
+	sf::Texture button1;
+	sf::Sprite lvl1button;
+	sf::Texture grid;
+	sf::Sprite playGrid;
+
+	/*
+	gameState will represent which screen the window will be showing
+	1 = title screen
+	2 = game board
+	3 = game board (in memo mode)
+	*/
+	int gameState;
 
 	//Private Functions
 	void initVar();
@@ -34,6 +49,7 @@ public:
 	const bool isRunning() const;
 
 	//Functions
+	void renderScreen();
 	void pollEvents();
 	void update();
 	void render();
